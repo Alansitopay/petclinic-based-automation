16a28ba1e81bbec3f2675d3a57ae1925fcf86241
Alansitopay <derpidark@outlook.com>
2025-07-28 21:15:32 -0300

    Role test creado

--- src/test/java/org/springframework/samples/petclinic/RoleTest.java
+++ src/test/java/org/springframework/samples/petclinic/RoleTest.java
@@ -2,11 +2,26 @@ package org.springframework.samples.petclinic;
 
 import io.qameta.allure.*;
 import org.springframework.samples.petclinic.model.Role;
+import org.springframework.samples.petclinic.model.User;
 import org.testng.annotations.*;
+
+import java.util.Set;
+
 import static org.testng.Assert.*;
 
 @Epic("Model")
 @Feature("Role Model Unit Tests")
 public class RoleTest {
+    @Test
+    public void testGetAndSetters(){
+        User user = new User();
+        user.setUsername("user");
+        user.setPassword("password");
+        Role role = new Role();
+        role.setName("Admin");
+        role.setUser(user);
 
-} 
+        assertEquals(role.getUser(),user);
+        assertEquals(role.getName(),"Admin");
+    }
+}
