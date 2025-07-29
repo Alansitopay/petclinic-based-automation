c84fe76bf2a2cfae7f6369279831670dc1bb51b3
Alansitopay <derpidark@outlook.com>
2025-07-28 21:18:35 -0300

    Specialty test creado

--- src/test/java/org/springframework/samples/petclinic/SpecialtyTest.java
+++ src/test/java/org/springframework/samples/petclinic/SpecialtyTest.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic;
 
 import io.qameta.allure.*;
+import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.model.Specialty;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
@@ -8,5 +9,10 @@ import static org.testng.Assert.*;
 @Epic("Model")
 @Feature("Specialty Model Unit Tests")
 public class SpecialtyTest {
-
-} 
+    @Test
+    public void testSetTypeName (){
+        Specialty specialty = new Specialty();
+        specialty.setName("Dentist");
+        assertEquals(specialty.getName(),"Dentist");
+    }
+}
