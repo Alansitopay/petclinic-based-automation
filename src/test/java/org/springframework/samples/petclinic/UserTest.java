10b09c4d9346c19b89f109f644264dd148420f40
Alansitopay <derpidark@outlook.com>
2025-07-28 21:30:33 -0300

    User test creado faltan pruebas

--- src/test/java/org/springframework/samples/petclinic/UserTest.java
+++ src/test/java/org/springframework/samples/petclinic/UserTest.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic;
 
 import io.qameta.allure.*;
+import org.springframework.samples.petclinic.model.Role;
 import org.springframework.samples.petclinic.model.User;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
@@ -8,5 +9,15 @@ import static org.testng.Assert.*;
 @Epic("Model")
 @Feature("User Model Unit Tests")
 public class UserTest {
+    @Test
+    public void testGettersYSetters(){
+        User user = new User();
+        user.setUsername("user");
+        user.setPassword("password");
+        user.setEnabled(true);
 
-} 
+        assertEquals(user.getUsername(), "user");
+        assertEquals(user.getPassword(), "password");
+        assertEquals(user.getEnabled(), Boolean.TRUE);
+    }
+}
