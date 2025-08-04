e1822b3715384f6005b349bf57186f50ae2dd2d1
Alansitopay <derpidark@outlook.com>
2025-08-04 20:50:11 -0300

    Mock de person crado

--- src/test/java/org/springframework/samples/petclinic/PersonTest.java
+++ src/test/java/org/springframework/samples/petclinic/PersonTest.java
@@ -1,6 +1,9 @@
 package org.springframework.samples.petclinic;
 
 import io.qameta.allure.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.samples.petclinic.model.Person;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -9,7 +12,29 @@ import static org.testng.Assert.*;
 @Epic("Model")
 @Feature("Person Model Unit Tests")
 public class PersonTest {
+    @Mock
+    private Person person;
+    private AutoCloseable closeable;
 
+    @BeforeMethod
+    public void setUp(){
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+    @AfterMethod
+    public void tearDown () throws Exception {
+        closeable.close();
+    }
+
+
+    @Test
+    public void testPersonMock (){
+        Mockito.when(person.getFirstName()).thenReturn("Roberto");
+        Mockito.when(person.getLastName()).thenReturn("Gutierrez");
+        Assert.assertEquals(person.getFirstName(),"Roberto");
+        Assert.assertEquals(person.getLastName(),"Gutierrez");
+        Mockito.verify(person).getFirstName();
+        Mockito.verify(person).getLastName();
+    }
     @Test
     public void testGetFirstName (){
         Person person = new Person();
