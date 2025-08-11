397a1f7f315df6a12a92f290114679a57fb9c09e
Alansitopay <derpidark@outlook.com>
2025-08-11 19:10:37 -0300

    Data set test creado y funcionando en owner

--- src/test/java/org/springframework/samples/petclinic/OwnerTest.java
+++ src/test/java/org/springframework/samples/petclinic/OwnerTest.java
@@ -8,11 +8,40 @@ import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.model.Pet;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.testng.Assert.*;
 
 @Epic("Model")
 @Feature("Owner Model Unit Tests")
 public class OwnerTest {
+//IMPLEMENTANDO DATA SETS
+    public List<String[]> loadCsvData(String path) throws Exception {
+        List<String[]> data = new ArrayList<>();
+        for (String line : Files.readAllLines(Paths.get(path))) {
+            data.add(line.split(","));
+        }
+        return data;
+    }
+    @DataProvider(name = "ownersProvider")
+    public Object[][] ownersProvider() throws Exception {
+        List<String[]> data = loadCsvData("src/test/resources/owners.csv");
+        return data.toArray(new Object[0][]);
+    }
+    @Test(dataProvider = "ownersProvider")
+    public void ownerTestDataUsage(String firstName, String lastName) {
+        Owner owner = new Owner();
+        owner.setFirstName(firstName);
+        owner.setLastName(lastName);
+
+        assertEquals(owner.getFirstName(), firstName);
+        assertEquals(owner.getLastName(), lastName);
+    }
+
     @Mock
     private Owner owner;
 
