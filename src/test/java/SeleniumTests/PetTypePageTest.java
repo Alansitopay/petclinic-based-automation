8a100bab49b212f1c6e061af4d772f4bdca2910a
Alansitopay <derpidark@outlook.com>
2025-08-24 14:26:30 -0300

    anotaciones agregadas

--- src/test/java/SeleniumTests/PetTypePageTest.java
+++ src/test/java/SeleniumTests/PetTypePageTest.java
@@ -2,13 +2,12 @@ package SeleniumTests;
 
 import ServicesApi.Base;
 import org.springframework.samples.petclinic.selenium.pages.PetTypes;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PetTypePageTest extends Base {
+public class TestPetTypePage extends Base {
 
     @Test
-    public void addPetType() throws InterruptedException {
+    public void TestAddPetType() throws InterruptedException {
         PetTypes petTypes = homePage.clickPetTypes();
         Thread.sleep(1000);
         petTypes.fillForm("perro");
